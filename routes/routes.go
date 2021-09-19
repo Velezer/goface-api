8848d59813208d32290d45305a8ba5b562477093
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-19 16:31:34 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,11 @@ import (
 )
 
 func Init(e *echo.Echo, h handler.Handler) {
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(200, map[string]string{
+			"message": "see?",
+		})
+	})
 
 	e.POST("api/face/find", h.Find, mymiddleware.JWTAuth)
 	e.POST("api/face/register", h.Register, mymiddleware.JWTAuth)
@@ -16,7 +21,6 @@ func Init(e *echo.Echo, h handler.Handler) {
 	e.GET("api/face", h.FaceAll, mymiddleware.JWTAuth)
 	e.GET("api/face/:id", h.FaceId, mymiddleware.JWTAuth)
 	e.DELETE("api/face/:id", h.Delete, mymiddleware.JWTAuth)
-
 
 	e.POST("jwt/login", h.JWTLogin)
 	e.POST("jwt/register", h.JWTRegister)
