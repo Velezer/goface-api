fd99dacb3e5d02910e69348e8bbb5b9642a56fd3
Arief Syaifuddin <asvelezer@gmail.com>
2021-09-23 16:33:56 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/Kagami/go-face"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 	"goface-api/database"
 	"goface-api/handler"
 	"goface-api/helper"
 	"goface-api/routes"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/Kagami/go-face"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 var (
@@ -39,15 +41,16 @@ func main() {
 	h := handler.Handler{Rec: rec, DB: db}
 	e := echo.New()
 
+	
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin},
+		AllowMethods: []string{http.MethodOptions, http.MethodConnect, http.MethodPost, http.MethodGet, http.MethodHead, http.MethodPut, http.MethodDelete},
+	}))
+	
 	routes.Init(e, h)
 
-	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	// 	AllowOrigins: []string{"*"},
-	// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin},
-	// 	AllowMethods: []string{http.MethodOptions, http.MethodConnect, http.MethodPost, http.MethodGet, http.MethodHead, http.MethodPut, http.MethodDelete},
-	// }))
-	e.Use(middleware.CORS())
-
+	
 	port := os.Getenv("PORT")
 	e.Logger.Fatal(e.Start(":" + port))
 
