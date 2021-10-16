e3d9c83e8ea00e4487a03025f0a08722bc0c99de
Arief Syaifuddin <asvelezer@gmail.com>
2021-10-16 07:56:01 +0700

    subtest

--- tests/handler/face.handler_test.go
+++ tests/handler/face.handler_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 )
 
-func TestHandler_Delete_Happy(t *testing.T) {
+func TestHandler_Delete(t *testing.T) {
 	req := httptest.NewRequest(http.MethodDelete, "/api/face/:id", nil)
 	rec := httptest.NewRecorder()
 
@@ -20,78 +20,57 @@ func TestHandler_Delete_Happy(t *testing.T) {
 	c.SetParamNames("id")
 	c.SetParamValues("23124112312")
 
-	repo := new(mymock.MockRepoFace)
-	repo.On("DeleteId", "23124112312").Return(nil)
-
-	h.DBRepo = &database.DBRepo{
-		RepoFace: repo,
-	}
-	// Assertions
-	if assert.NoError(t, h.Delete(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-	}
-}
-func TestHandler_Delete_DelError(t *testing.T) {
-
-	req := httptest.NewRequest(http.MethodDelete, "/api/face/:id", nil)
-
-	rec := httptest.NewRecorder()
-
-	c := e.NewContext(req, rec)
-	c.SetParamNames("id")
-	c.SetParamValues("23124112312")
-
 	repoFace := new(mymock.MockRepoFace)
-	repoFace.On("DeleteId", "23124112312").Return(errors.New("delete id error"))
 
 	h.DBRepo = &database.DBRepo{
 		RepoFace: repoFace,
 	}
+	t.Run("Delete Happy", func(t *testing.T) {
+		repoFace.On("DeleteId", "23124112312").Return(nil).Once()
 
-	// Assertions
-	errHandler := h.Delete(c).(*echo.HTTPError)
-	assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
+		if assert.NoError(t, h.Delete(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
+	})
+	t.Run("Delete DBError", func(t *testing.T) {
+		repoFace.On("DeleteId", "23124112312").Return(errors.New("delete id error")).Once()
+
+		// Assertions
+		errHandler := h.Delete(c).(*echo.HTTPError)
+		assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
+	})
+
 }
 
-func TestHandler_FaceAll_Happy(t *testing.T) {
-
+func TestHandler_FaceAll(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "/api/face/", nil)
 	rec := httptest.NewRecorder()
 
 	c := e.NewContext(req, rec)
-
 	repoFace := new(mymock.MockRepoFace)
-	repoFace.On("FindAll").Return([]models.Face{faceData}, nil)
-
 	h.DBRepo = &database.DBRepo{
 		RepoFace: repoFace,
 	}
+	t.Run("FaceAll Happy", func(t *testing.T) {
+		repoFace.On("FindAll").Return([]models.Face{faceData}, nil).Once()
 
-	// Assertions
-	if assert.NoError(t, h.FaceAll(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-	}
-}
-func TestHandler_FaceAll_FindAllErr(t *testing.T) {
+		// Assertions
+		if assert.NoError(t, h.FaceAll(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
+	})
 
-	req := httptest.NewRequest(http.MethodGet, "/api/face/", nil)
-	rec := httptest.NewRecorder()
+	t.Run("FaceAll FindAllErr", func(t *testing.T) {
+		repoFace.On("FindAll").Return([]models.Face{faceData}, errors.New("FindAll err")).Once()
 
-	c := e.NewContext(req, rec)
+		// Assertions
+		errHandler := h.FaceAll(c).(*echo.HTTPError)
+		assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
+	})
 
-	repoFace := new(mymock.MockRepoFace)
-	repoFace.On("FindAll").Return([]models.Face{faceData}, errors.New("FindAll err"))
-
-	h.DBRepo = &database.DBRepo{
-		RepoFace: repoFace,
-	}
-
-	// Assertions
-	errHandler := h.FaceAll(c).(*echo.HTTPError)
-	assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
 }
 
-func TestHandler_FaceId_Happy(t *testing.T) {
+func TestHandler_FaceId(t *testing.T) {
 
 	req := httptest.NewRequest(http.MethodGet, "/api/face/:id", nil)
 	rec := httptest.NewRecorder()
@@ -99,36 +78,24 @@ func TestHandler_FaceId_Happy(t *testing.T) {
 	c := e.NewContext(req, rec)
 	c.SetParamNames("id")
 	c.SetParamValues("13213213")
-
 	repoFace := new(mymock.MockRepoFace)
-	repoFace.On("FindById", "13213213").Return([]models.Face{faceData}, nil)
-
 	h.DBRepo = &database.DBRepo{
 		RepoFace: repoFace,
 	}
 
-	// Assertions
-	if assert.NoError(t, h.FaceId(c)) {
-		assert.Equal(t, http.StatusOK, rec.Code)
-	}
-}
-func TestHandler_FaceId_FindByIdErr(t *testing.T) {
+	t.Run("FaceId Happy", func(t *testing.T) {
+		repoFace.On("FindById", "13213213").Return([]models.Face{faceData}, nil).Once()
+		// Assertions
+		if assert.NoError(t, h.FaceId(c)) {
+			assert.Equal(t, http.StatusOK, rec.Code)
+		}
+	})
 
-	req := httptest.NewRequest(http.MethodGet, "/api/face/:id", nil)
-	rec := httptest.NewRecorder()
+	t.Run("FaceId FindByIdErr", func(t *testing.T) {
+		repoFace.On("FindById", "13213213").Return([]models.Face{faceData}, errors.New("FindByIdErr")).Once()
+		// Assertions
+		errHandler := h.FaceId(c).(*echo.HTTPError)
+		assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
+	})
 
-	c := e.NewContext(req, rec)
-	c.SetParamNames("id")
-	c.SetParamValues("13213213")
-
-	repoFace := new(mymock.MockRepoFace)
-	repoFace.On("FindById", "13213213").Return([]models.Face{faceData}, errors.New("FindByIdErr"))
-
-	h.DBRepo = &database.DBRepo{
-		RepoFace: repoFace,
-	}
-
-	// Assertions
-	errHandler := h.FaceId(c).(*echo.HTTPError)
-	assert.Equal(t, http.StatusInternalServerError, errHandler.Code)
 }
